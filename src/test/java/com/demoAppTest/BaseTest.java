3609049e0160f1e24ecc2c37d428ff760c413620
EyadMuhamed <116434993+EyadMuhamed@users.noreply.github.com>
2023-10-05 19:24:19 +0300

    #second commit

--- src/test/java/com/demoAppTest/BaseTest.java
+++ src/test/java/com/demoAppTest/BaseTest.java
@@ -5,7 +5,6 @@ import io.appium.java_client.android.options.UiAutomator2Options;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
 import io.appium.java_client.service.local.AppiumServiceBuilder;
 import org.testng.annotations.*;
-
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
